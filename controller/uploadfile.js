2edcdb2e05a87bef7c206d99d437a7e88ab9036f
sandeep23072000 <ssanini70@gmail.com>
2023-04-03 10:15:46 +0530

    add populate in uploadfile

--- controller/uploadfile.js
+++ controller/uploadfile.js
@@ -1,24 +1,36 @@
 const mongoose = require('mongoose');
+const { callbackPromise } = require('nodemailer/lib/shared');
 const fileModel = require('../models/uploadfile');
+const user = require('../models/user');
+const userModel = require('../models/user');
 
 const LiveController = {};
 LiveController.fileupload = async (req, res) => {
     try {
         console.log(req.files, "dsfdfsdfaaaaaaaaaaaaaaaaaaaaa")
         if (req.files.image) {
-            req.body.image = req.files.image[0].path
-           return res.status(201).json({ message: 'uploaded successfully', data: req.body})
-        }
+            req.body.image = req.files.image[0].path;
+
             const posts = new fileModel(req.body);
             await posts.save();
+
+            const user = await posts.populate("userId")
+                .then(p => console.log(p))
+                .catch(error => console.log(error));
+
             return res.status(200).send({
+                message: 'uploaded successfully',
                 success: true,
-                data: posts
-            })
-        
+                data: posts,
+                user: user
+            });
+        }
+
+
+
     } catch (error) {
         console.log(error);
-       return res.status(500).json({ message: 'Somthing went wrong' });
+        return res.status(500).json({ message: 'Somthing went wrong' });
     }
 }
 
--- models/uploadfile.js
+++ models/uploadfile.js
@@ -1,9 +1,14 @@
 
 const mongoose = require('mongoose');
-
+// const user = mongoose.model('User', userSchema);
 const fileSchema = new mongoose.Schema({
     image:{
         type: String,
+        // required: true
+    },
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
         // required: true
     },
 });
